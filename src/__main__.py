e158a3ded5335c049ab9e2c54e1cca1f4a76feb8
Bauti Peirone <bautista.peirone@gmail.com>
2021-11-16 20:35:37 -0300

    License Plates Tracker for Python
    This python module provides functions and tools for working with license plates and its patterns,
    like calculating the amount of combinations possible for a specific type of plate,
    or getting the nth license plate for a given pattern.
    
    Standard plate patterns for Europe countries, US states and Latin America countries are
    provided within the module.

---
+++ src/__main__.py
@@ -0,0 +1,48 @@
+import argparse
+import json
+import pathlib
+import lic_plates as lp
+
+PATH = pathlib.Path(__file__)
+
+with open(PATH.parent / "prog_info.json") as f:
+    json_file = json.loads(f.read())
+    
+    programme_info = json_file["License Plates"]["Programme Info"]
+    prog_usage = programme_info["Programme Usage"]
+    prog_description = programme_info["Programme Description"]
+    prog_epilog = programme_info["Programme Epilog"]
+
+    help_str = json_file["License Plates"]["Function Help Strings"]
+
+
+parser = argparse.ArgumentParser(
+    prog = "License Plates",
+    usage = prog_usage,
+    description= prog_description,
+    add_help = True,
+    epilog=prog_epilog,
+)
+
+
+parser.add_argument(
+    dest="command",
+)
+
+parser.add_argument(
+    dest="args",
+)
+
+parser.add_argument(
+    "-v", "--version",
+)
+
+
+def main() -> None:
+    user_input = parser.parse_args()
+    args = vars(user_input)
+    return
+
+
+if __name__ == "__main__":
+    main()
